aab01e64137070ff9577b4fef5e141d8e58665b9
ajaydhakal01 <ajaydhakal110@gmail.com>
2023-11-18 11:39:23 +0545

    v1

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
-mongoose.set('strictQuery', true);
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(static_path));
